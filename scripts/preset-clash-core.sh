5f3190788a6c56683a757a1cbeadd1d020ed5776
Aeros Lubis <aeros.lubis@gmail.com>
2021-10-02 15:10:59 +0700

    Update preset-clash-core.sh

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -9,7 +9,9 @@
 # Blog: https://mlapp.cn
 #=================================================
 
-mkdir -p files/etc/openclash/core
+clash_core_path="/amlogic-s9xxx/common-files/files/etc/openclash/core"
+
+mkdir -p $clash_core_path
 
 open_clash_premium_url=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/tags/premium | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
 open_clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
@@ -17,10 +19,10 @@ open_clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/
 clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
 clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
 
-wget -qO- $open_clash_premium_url | gunzip -c > files/etc/openclash/core/clash
-wget -qO- $open_clash_main_url | tar xOvz > files/etc/openclash/core/clash_original
-# wget -qO- $offical_clash_main_url | gunzip -c > files/etc/openclash/core/clash
-wget -qO- $clash_tun_url | gunzip -c > files/etc/openclash/core/clash_tun
-wget -qO- $clash_game_url | tar xOvz > files/etc/openclash/core/clash_game
+wget -qO- $open_clash_premium_url | gunzip -c > $clash_core_path/clash
+wget -qO- $open_clash_main_url | tar xOvz > $clash_core_path/clash_original
+# wget -qO- $offical_clash_main_url | gunzip -c > $clash_core_path/clash
+wget -qO- $clash_tun_url | gunzip -c > $clash_core_path/clash_tun
+wget -qO- $clash_game_url | tar xOvz > $clash_core_path/clash_game
 
-chmod +x files/etc/openclash/core/clash*
+chmod +x $clash_core_path/clash*
